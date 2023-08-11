f34a19197a4ef3c1f97bd3344d1f103666c1bd67
YshikageOne <124365039+YshikageOne@users.noreply.github.com>
2023-08-12 02:22:21 +0800

    Add files via upload

---
+++ Week1/CelsiusToFahrenheit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1.Week1 //My first C# code after years of not practicing
+{
+    internal class CelsiusToFahrenheit
+    {
+        static void Main(string[] args)
+        {
+            //initialize them variables
+            double celsius, fahrenheit;
+            Console.Write("Enter your celsius temperature: ");
+            celsius = Convert.ToDouble(Console.ReadLine()); //scanner class that would take the input as variable
+
+            //Convertion
+            fahrenheit = celsius * 9 / 5 + 32;
+
+            //Display output
+            Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit:F2}");
+            Console.Beep();
+
+        }
+    }
+}
---
+++ Week1/EvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1.Week1
+{
+    internal class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            int userNum;
+            Console.Write("Enter your number: ");
+            userNum = Convert.ToInt32(Console.ReadLine());
+
+            if (userNum % 2 == 0)
+            {
+                Console.WriteLine(userNum + " is an even number.");
+            }
+            else
+            {
+                Console.WriteLine(userNum + " is an odd number.");
+            }
+        }
+    }
+}
