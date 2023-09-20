4c8d392b12ccc4034ce49eba6d412c82aa0b4699
Clyde Allen T. Yu <124365039+YshikageOne@users.noreply.github.com>
2023-09-20 14:02:10 +0800

    Add files via upload

---
+++ Week7/Simple Calculator/SimpleCalculator/SimpleCalculatorMain.cs
@@ -0,0 +1,18 @@
+﻿using SchoolStuff2.Week7.test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolStuff2.Week7.SimpleCalculator
+{
+    internal static class SimpleCalculatorMain
+    {
+        static void Main()
+        {
+           ApplicationConfiguration.Initialize();
+           Application.Run(new SimpleCalculatorFrame());
+        }
+    }
+}
